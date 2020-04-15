cc8349717a860725248a1df50d25646c559d4c40
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-15 13:44:45 -0700

    Number function works

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace intro_to_csharp
         static void Main(string[] args)
         {
 
+            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+
+            var result = 0.0;
+            foreach(var number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine(result);
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
