31396eecb7af582ded75ed5a70759e967e490609
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-15 14:13:50 -0700

    Create lists and show average of grades list.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace intro_to_csharp
 {
@@ -7,14 +8,18 @@ namespace intro_to_csharp
         static void Main(string[] args)
         {
 
-            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            // var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
 
             var result = 0.0;
-            foreach(var number in numbers)
+            foreach(var number in grades)
             {
                 result += number;
             }
-            Console.WriteLine(result);
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
 
             if(args.Length > 0)
             {
