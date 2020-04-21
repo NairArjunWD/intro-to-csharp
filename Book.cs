d294a042110b6030ba7cabf0c14ca2c371d28a4d
Arjun Nair <nair.arjun47@yahoo.com>
2020-04-20 20:42:08 -0700

    Add constructor

--- Book.cs
+++ Book.cs
@@ -1,10 +1,19 @@
+using System.Collections.Generic;
+
 namespace intro_to_csharp
 {
     class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+        
         public void AddGrade(double grade)
         {
-            
+            grades.Add(grade);
         }
+
+        List<double> grades;
     }
 }
